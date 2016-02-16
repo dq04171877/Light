4b43b10a21b69de75b494d68e6784619b5798189
dq04171877 <488650965@qq.com>
2016-02-16 10:37:36 +0800

    Torch on and off

---
+++ src/com/example/light/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.light;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+	private Camera camera = null;
+	private Parameters parameters = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		// 直接开启
+		camera = Camera.open();
+		parameters = camera.getParameters();
+		parameters.setFlashMode(Parameters.FLASH_MODE_TORCH);// 开启
+		camera.setParameters(parameters);
+		try {
+			Thread.currentThread();
+			Thread.sleep(500);//阻断2秒
+			} catch (InterruptedException e) {
+			e.printStackTrace();
+			}
+		parameters.setFlashMode(Parameters.FLASH_MODE_OFF);
+		camera.setParameters(parameters);
+		camera.release();
+		
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
